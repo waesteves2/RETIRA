ecad2ec685de6caaa112343217159e67d9543ef2
Elton Esteves <121466168+waesteves2@users.noreply.github.com>
2024-07-19 14:44:48 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const https = require('https');
 const WebSocket = require('ws');
 
-// Configura o servidor HTTPS
+// Configuração do servidor HTTPS
 const server = https.createServer({
   cert: fs.readFileSync('path/to/your/certificate.crt'),
   key: fs.readFileSync('path/to/your/private.key')
@@ -15,10 +15,9 @@ wss.on('connection', (ws) => {
 
   ws.on('message', (message) => {
     console.log(`Recebido: ${message}`);
-    const jsonMessage = JSON.stringify({ message });
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(jsonMessage);
+        client.send(message);
       }
     });
   });
